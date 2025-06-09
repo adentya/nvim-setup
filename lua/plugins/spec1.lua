4ae4b6aab40bdc4f8ab22cfe48db7ae6c27be00a
Adentya <adentya.maryo@gmail.com>
2025-06-09 14:12:32 +0700

    Update config

--- lua/plugins/spec1.lua
+++ lua/plugins/spec1.lua
@@ -401,6 +401,10 @@ return {
             "nvim-tree/nvim-web-devicons",
         },
         config = function()
+            vim.cmd [[
+                highlight! link NvimTreeOpenedFileName TermCursor
+            ]]
+
             require("nvim-tree").setup {
                 sort = {
                     sorter = "case_sensitive",
@@ -415,11 +419,24 @@ return {
                 git = {
                     enable = true,
                     ignore = false,
-                }
+                },
+                update_focused_file = {
+                    enable = true,
+                    update_cwd = true, -- optional: updates Neovim's working dir
+                },
+                renderer = {
+                    highlight_opened_files = "all", -- options: "none", "icon", "name", "all"
+                },
             }
 
             vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
             vim.keymap.set("n", "<leader>tf", ":NvimTreeFindFile<CR>", { noremap = true, silent = true })
+            vim.keymap.set("n", "<leader>ts2", ":NvimTreeResize 20<CR>", { noremap = true, silent = true })
+            vim.keymap.set("n", "<leader>ts3", ":NvimTreeResize 30<CR>", { noremap = true, silent = true })
+            vim.keymap.set("n", "<leader>ts4", ":NvimTreeResize 40<CR>", { noremap = true, silent = true })
+            vim.keymap.set("n", "<leader>ts5", ":NvimTreeResize 50<CR>", { noremap = true, silent = true })
+
+
         end,
     },
     {
