b46189096b745e8737cbaa29e74170edde1132d0
Adentya <adentya.maryo@gmail.com>
2025-06-26 01:18:10 +0700

    Update config

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -5,6 +5,7 @@ return {
             'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
             'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
         },
+        version = '^1.0.0', -- optional: only update when a new 1.x version is released
         init = function() 
             vim.g.barbar_auto_setup = false 
 
@@ -41,6 +42,11 @@ return {
                 buffer_index = true,
             },
         },
-        version = '^1.0.0', -- optional: only update when a new 1.x version is released
+        config = function()
+            require('barbar').setup {
+                minimum_padding = 2,
+                maximum_padding = 2,
+            }
+        end,
     },
 }
